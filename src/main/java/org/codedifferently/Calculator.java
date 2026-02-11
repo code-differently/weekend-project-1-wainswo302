0bc3115792c57387eb892092f60d153878ee19a0
wilfredainsworth <wainsworth261@gmail.com>
2026-02-10 23:36:15 -0500

    Fully functioning program.

--- src/main/java/org/codedifferently/Calculator.java
+++ src/main/java/org/codedifferently/Calculator.java
@@ -1,35 +1,68 @@
 package org.codedifferently;
 
 import java.util.Random;
+import java.util.Scanner;
 
 
 public class Calculator {
     static double calculatedTax;
     static Random r = new Random();
+    static Scanner sc = new Scanner (System.in);
 
-    public static double calcTax (double billAmount, double tax){
-        calculatedTax = billAmount * (tax / 100);
-        return calculatedTax;
+    public static double calcTax(double subtotal, double tax) {
+        calculatedTax = subtotal * tax;
+        return roundTotals(calculatedTax);
     } // return random tax rate between 0-10%
+
     public static double returnRandomTaxRate() {
-        double taxRate = r.nextDouble(10);
-        return taxRate/100;
+        double taxRate = r.nextDouble(1, 10);
+        return taxRate / 100;
     }//return actual taxes to be billed
-    public static double calculateTax(double totalPrice, double taxRate) {
-        return totalPrice * (taxRate / 100);
-    }
-    public static double calculateSubtotal(double appetizer,double entree, double drink) {
-        return appetizer+entree+drink;
-    }
-    public static double calculateTrueTotal(double subtotal, double tax){
-        return subtotal+tax;
-    }
-    public static double calculateBudget(double userBudget, double trueTotal){
-        return userBudget+trueTotal;
+
+    public static double calculateSubtotal(double appetizer, double entree, double drink) {
+        double subtotal = appetizer + entree + drink;
+//        if (discount > 0) {
+//            subtotal = subtotal * discount;
+//        } else {
+//        }
+        return subtotal;
     }
 
-
-
-
+    public static double calculateBudget(double userBudget, double trueTotal) {
+        return userBudget - trueTotal;
     }
 
+    public static double roundTotals(double total) {
+        return Math.round(total * 100.00) / 100.00;
+    }
+
+    public static void completeTransaction(double budgetRemaining) {
+        if (budgetRemaining >= 0) {
+            System.out.println("You can afford this transaction and have $" + roundTotals(budgetRemaining) + " remaining.");
+        } else {
+            System.out.println("You cannot afford this transaction! \nYou are short by $" + roundTotals(Math.abs(budgetRemaining)));
+        }
+    }
+
+    public static double validateCoupon() {
+        String couponCode = sc.nextLine();
+        if ((couponCode).equalsIgnoreCase("quarterback")) {
+            System.out.println("Coupon code " + "\"" + couponCode + "\" accepted!\"You have saved 25%");
+            return 0.25;
+        } else if ((couponCode).equalsIgnoreCase("freddysemployee")) {
+            System.out.println("Coupon code " + "\"" + couponCode + "\" accepted!\"You have saved 50%");
+            return 0.50;
+        } else {
+            System.out.println("Coupon code " + "\"" + couponCode + "\" not found!\"No discount applied. :(");
+            return 0.0;
+        }
+    }
+
+    public static double applyDiscount(double subtotal, double discount) {
+        return roundTotals(subtotal*(1-discount));
+        }
+    public static double calculateTrueTotal(double subtotal, double taxCharged) {
+        return subtotal + taxCharged;
+    }
+
+    }
--- src/main/java/org/codedifferently/Main.java
+++ src/main/java/org/codedifferently/Main.java
@@ -1,18 +1,40 @@
 package org.codedifferently;
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
-    static void main() {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name: ");
+
+        System.out.println("WELCOME TO FATBOY FREDDY'S \nHome of Fat Fingers and Fried Footlongs!");
+        System.out.println("Enter your name: " );
         String userName = sc.nextLine();
         System.out.println("What is your budget?");
         double userBudget = sc.nextDouble();
 
-        String couponCode;
+        double app1 = RandomGenerator.generateAppetizerPrice();
+        double ent1 = RandomGenerator.generateEntreePrice();
+        double drink1 = RandomGenerator.generateDrinkPrice();
+        System.out.println("Appetizer costs: $" + Calculator.roundTotals(app1));
+        System.out.println("Entree costs: $" + Calculator.roundTotals(ent1));
+        System.out.println("Drink costs: $" + Calculator.roundTotals(drink1));
 
+        double subtotal = Calculator.calculateSubtotal(app1, ent1, drink1);
+        System.out.println("Here is your subtotal: $" + Calculator.roundTotals(subtotal));
 
+        System.out.println("Do you have a coupon code to enter?");
 
-        }
+        double discount = Calculator.validateCoupon();
+        double discountedSubtotal = Calculator.applyDiscount(subtotal,discount);
+        System.out.println("Here's your discounted pre-tax total: $"+ discountedSubtotal);
+        double randomTaxRate = Calculator.returnRandomTaxRate();
+        double taxCharged = Calculator.calcTax(discountedSubtotal,randomTaxRate);
+        System.out.println("Sales tax: $" + taxCharged + " (Simulated tax rate: "+Calculator.roundTotals(randomTaxRate)+"%)");
+        double finalTotal = Calculator.calculateTrueTotal(discountedSubtotal,taxCharged);
+        System.out.println("Total due: $" + Calculator.roundTotals(finalTotal));
+        double finalBudget = Calculator.calculateBudget(userBudget,finalTotal);
+
+        Calculator.completeTransaction(finalBudget);
+        System.out.println(RandomGenerator.generateReceiptCode(userName));
+
+    }
     }
